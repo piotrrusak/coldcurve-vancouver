8551998d2232610604af8593b707493f14a6f256
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-04-23 10:56:06 +0200

    WIP: Hud mod

--- scripts/Chud/hud.gd
+++ scripts/Chud/hud.gd
@@ -4,6 +4,8 @@ signal start_game
 
 func show_game_over():
 	$StartButton.show()
+	$OptionsButton.show()
+	$ExitButton.show()
 	$GameOverLabel.show()
 
 func update_score(score):
@@ -11,5 +13,13 @@ func update_score(score):
 
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$OptionsButton.hide()
+	$ExitButton.hide()
 	$GameOverLabel.hide()
 	start_game.emit()
+
+func _on_options_button_pressed():
+	pass
+
+func _on_exit_button_pressed():
+	get_tree().quit()
