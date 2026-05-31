0647a75b4fe48b79ffbcfdf45702d56252e3e3b6
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-05-31 13:46:38 +0200

    level 8 map

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -31,6 +31,10 @@ func _on_level7_pressed():
 	level_selected.emit(6)
 	queue_free()
 
+func _on_level8_pressed():
+	level_selected.emit(7)
+	queue_free()
+
 func _on_back_pressed():
 	back.emit()
 	queue_free()
